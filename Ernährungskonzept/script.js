4239240a9dfdfca1f219b12e952c4fddabbe9608
0xVato <man-wei@gmx.de>
2022-01-25 05:06:55 +0100

    Update Kommentare

--- Ernährungskonzept/script.js
+++ Ernährungskonzept/script.js
@@ -1,40 +1,65 @@
 /* eslint-disable no-undef */
-/* function calculatekcal() {
-  var selectedPerformance = document.querySelector(
-    'input[name="performance"]:checked',
-  ).value;
-  var selectedGewichtmanagement = document.querySelector(
-    'input[name="gewichtmanagement"]:checked',
-  ).value;
-  var total =
-    parseInt(selectedPerformance) + parseInt(selectedGewichtmanagement);
-  document.getElementById('rechnung').value = total;
-} */
 
+//Toggle-Funktion des Darkmodes
+//
+//
+$('#darktoggle').click(function () {
+  $('body').toggleClass('dark');
+  $('.ernaehrungstool').toggleClass('dark');
+  $('.navbar').toggleClass('dark');
+  $('.navbar-links').toggleClass('dark');
+  $('.tool-steps').toggleClass('dark');
+  $('.mehrauswahl').toggleClass('dark');
+});
+
+//Stickybar CSS nach Scrollen
+//
+//
+window.onscroll = function () {
+  stickybar();
+};
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+function stickybar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+//Berechnung der Input-Values anhand der Namen + Überprüfung ob checked
+//
+//
 $('input').change(function () {
-  // run anytime the value changes
-
+  // jedesmal ausführen, wenn sich ein Input-Wert ändert
   var performancevalue = null;
   var gewichtmanagementvalue = null;
-  /*   var firstValue = Number($('#first').val());
-  var secondValue = Number($('#second').val()); */
 
+  //Falls mit normalen Input-Felndern gearbeitet werden soll
+  //var firstValue = Number($('#first').val());
+  //var secondValue = Number($('#second').val()); */
+
+  //Inputfeldname = checked => Value zu "Xvalue"
   if ($("input[name='performance']").is(':checked')) {
     performancevalue = Number($("input[name='performance']:checked").val());
   }
-
   if ($("input[name='gewichtmanagement']").is(':checked')) {
     gewichtmanagementvalue = Number(
       $("input[name='gewichtmanagement']:checked").val(),
     );
   }
-
-  $('#rechnungohneinput').html(performancevalue + gewichtmanagementvalue); // add them and output it
+  // ZWei Berechnungsbeispiele, einmal als HTML-Text, einmal in Input-Feld
+  $('#rechnungohneinput').html(performancevalue + gewichtmanagementvalue);
   document.getElementById('rechnung').value =
     performancevalue + gewichtmanagementvalue;
-  // add them and output it
 });
 
+//Wenn Input-Feld gedrückt wird...
+//mit z.B. der ID "ziel1", dann..
+//zeige / verstecke diverse Elemente oder unchecke nicht sichtbare Inputs
+//
+//
 $('input[name="ziel"]').click(function () {
   var inputValue = $(this).attr('id');
   if (inputValue == 'ziel1') {
@@ -61,6 +86,9 @@ $('input[name="ziel"]').click(function () {
   }
 });
 
+//zeige Kalorienverbrauchfrage, sobald ein ELement aus den 3 Ziel-Gruppen ausgewählt wurde
+//
+//
 $('input[type="radio"]').click(function () {
   var inputValue = $(this).attr('name');
   if (inputValue == 'performance') {
@@ -72,19 +100,9 @@ $('input[type="radio"]').click(function () {
   }
 });
 
-$('#section2-1').click(function () {
-  $('.step1').addClass('highlight');
-  $('.step2, .step3').removeClass('highlight');
-  $('#section-ziel').show();
-  $('#section-koerper, #section-aktivitaet').hide();
-  $([document.documentElement, document.body]).animate(
-    {
-      scrollTop: $('#navbar').offset().top,
-    },
-    500,
-  );
-});
-
+//Buttons zum Seitenwechsel und ändern der Step-CSS
+//
+//Weiter Button von Sektion 1 zu 2
 $('#section1-2').click(function () {
   $('.step2').addClass('highlight');
   $('.step1, .step3').removeClass('highlight');
@@ -97,12 +115,12 @@ $('#section1-2').click(function () {
     500,
   );
 });
-
-$('#section2-3').click(function () {
-  $('.step3').addClass('highlight');
-  $('.step1, .step2').removeClass('highlight');
-  $('#section-aktivitaet').show();
-  $('#section-ziel, #section-koerper').hide();
+//Zurück Button von Sektion 2 zu 1
+$('#section2-1').click(function () {
+  $('.step1').addClass('highlight');
+  $('.step2').removeClass('highlight');
+  $('#section-ziel').show();
+  $('#section-koerper').hide();
   $([document.documentElement, document.body]).animate(
     {
       scrollTop: $('#navbar').offset().top,
@@ -110,32 +128,16 @@ $('#section2-3').click(function () {
     500,
   );
 });
-
-/* const darktoggle = document.getElementById('darktoggle');
-darktoggle.addEventListener('change', () => {
-  // change Theme //
-  document.body.classList.toggle('dark');
-}); */
-
-$('#darktoggle').click(function () {
-  $('body').toggleClass('dark');
-  $('.ernaehrungstool').toggleClass('dark');
-  $('.navbar').toggleClass('dark');
-  $('.navbar-links').toggleClass('dark');
-  $('.tool-steps').toggleClass('dark');
-  $('.mehrauswahl').toggleClass('dark');
+//Weiter Button von Sektion 2 zu 3
+$('#section2-3').click(function () {
+  $('.step3').addClass('highlight');
+  $('.step2').removeClass('highlight');
+  $('#section-aktivitaet').show();
+  $('#section-koerper').hide();
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $('#navbar').offset().top,
+    },
+    500,
+  );
 });
-
-// When the user scrolls the page, execute stickybar
-window.onscroll = function () {
-  stickybar();
-};
-var navbar = document.getElementById('navbar');
-var sticky = navbar.offsetTop;
-function stickybar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-}
